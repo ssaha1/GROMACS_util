6f7cdae533f31df2b9566a080ab6afed3264fde2
ssaha1 <ssaumitra@gmail.com>
2020-05-29 14:34:49 +1000

    added python script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GROMACS_util
- 
+
+This is a PDB to xyz conversion tool. 
+Input: Any GROMACS created molecular structure in PDB format
+Output:  Molecular structure in xyz format
+
+
+usage: pdb2xyz.py [-h] [-i INPFILE] [-o OUTFILE]
+
+Convert .pdb file into .xyz
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -i INPFILE  name of input in PDB format
+  -o OUTFILE  name of output in XYZ format
+
---
+++ pdb2xyz.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+'''
+Created on 7 Jun 2019
+
+@author: saumitras '''
+from argparse import ArgumentParser
+'''
+Convert Gromacs created PDB files into xyz fromat
+'''
+
+def ReadPDB(args):
+    fname = args.inpfile
+    atomno = 0
+    with open(fname, 'r') as pdbfile:
+        for line in pdbfile:
+            if line[:4] == 'ATOM' or line[:6] == "HETATM":
+                atomno += 1
+            else:
+                pass
+
+    with open(fname, 'r') as pdbfile, open(args.outfile, 'w') as xyzfile:
+        xyzfile.write(str(atomno) + '\n')
+        xyzfile.write(fname + '\n')
+        for line in pdbfile:
+            if line[:4] == 'ATOM' or line[:6] == "HETATM":
+                # Split the line
+                splitted_line_xyz = [line[77:79].rstrip(), line[30:38], line[38:46], line[46:54]]
+                # print splitted_line_xyz
+                # To format again the pdb file with the fields extracted
+                xyzfile.write("%4s    %8s%8s%8s\n" % tuple(splitted_line_xyz), )
+
+
+if __name__ == '__main__':
+    # Initialize parser.
+    parser = ArgumentParser(description='Convert .pdb file into .xyz')
+    parser.add_argument('-i', dest="inpfile", type=str, help='name of input in PDB format')
+    parser.add_argument('-o', dest="outfile", type=str, help='name of output in XYZ format')
+    parser.set_defaults(func=ReadPDB)
+    #     ReadPDB('1grl.pdb')
+
+    args = parser.parse_args()
+    print(args)
+
+    try:
+        getattr(args, "func")
+    except AttributeError:
+        parser.print_help()
+        exit(0)
+    args.func(args)
+
+    pass
